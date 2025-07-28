13e7bd25d5d9fe91599898ed3bbd2feb92ab7237
pradeep <pradeepkudipudi05@gmail.com>
2025-07-28 10:26:13 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - S205</h1>
+      <p> ci/cd and cloud devops</p>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
     </div>
   );
